b9d8de2c00e31cdd7709aa93ae66f3aa9981d071
oskarlsson <oscar.carlsson@live.com>
2021-12-15 16:55:02 +0100

    tror

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Counter } from './Counter';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Counter } from "./Counter";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <Counter>
+            {({ count, setCount }) => (
+              <div>
+                {count}
+                <button onClick={() => setCount(count + 1)}>+</button>
+              </div>
+            )}
+          </Counter>
+          <p></p>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React, also Hello World!
-        </a>
-        <Counter>
-          {({ count, setCount }) => (
-            <div>
-              {count}
-              <button onClick={() => setCount(count + 1)}>+</button>
-            </div>)}
-        </Counter>
       </header>
     </div>
   );
--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,18 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 interface Props {
-    children: (data: {
-        count: number,
-        setCount: React.Dispatch<React.SetStateAction<number>>
-    }) => JSX.Element | null;
+  children: (data: {
+    count: number;
+    setCount: React.Dispatch<React.SetStateAction<number>>;
+  }) => JSX.Element | null;
 }
 
 export const Counter: React.FC<Props> = ({ children }) => {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    return (
-        <div>
-            {children({ count, setCount })}
-        </div>
-    );
-}
+  return <div>{children({ count, setCount })}</div>;
+};
